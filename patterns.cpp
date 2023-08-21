b761a450a0b12d3e008feeaf8e42fba3bb7b2712
Developer Baboo <babooheerani@gmail.com>
2023-08-21 20:41:42 +0500

    first commit

---
+++ patterns.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// This printing rectangle
+//  int row, col;
+//  cin>>row>>col;
+//  for(int i = 1;i<=row; i++){
+//  	for(int j = 1;j<=col;j++){
+//  		cout<<"*";
+//	  }
+//	  cout<<endl;
+//  }
+
+//Hollow Recntagle Pattern
+
+//int row, col;
+//cin>>row>>col;
+//for(int i = 1; i<=row; i++){
+//	for(int j =1;j<=col; j++){
+//		if(i==1 || i==row || j==1 || j==col ){
+//			cout<<"*";
+//		}else{
+//			cout<<" ";
+//		}
+//	}
+//	cout<<endl;
+//}
+
+
+//Inverted Half Pyramid
+
+//int n;
+//cin>>n;
+//for(int i = n; i>=1; i--){
+//	for(int j =1; j<=i;j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+//Half Pyramid After 180 Degree Rotation
+//int n;
+//cin>>n;
+//for(int i = 1; i<=n; i++ ){
+//	for(int j = 1; j<=n; j++){
+//		if(j<=n-i){ //n = 10, i = 1
+//			cout<<"  ";
+//		}else{
+//			cout<<"* ";
+//		}
+//	}
+//	cout<<endl;
+//}
+
+//Half Pyramidn Using number
+
+//int n;
+//cin>>n;
+//for(int i = 1; i<=n; i++){
+//	for(int j=1; j<=i;j++ ){
+//		cout<<i<<" ";
+//	}cout<<endl;
+//}
+
+//Floyd's Triangle
+//int n;
+//cin>>n;
+//int count = 1;
+//for(int i =1; i<=n; i++){
+//	for(int j = 1; j<=i; j++){
+//		cout<<count<<" ";
+//		count++;
+//	}
+//	cout<<endl;
+//}
+
+// Butterfuly Pattern
+
+//int n = 4;
+//cin>>n;
+//for(int i =1;i<=n; i++){
+//	for(int j =1; j<=i; j++){
+//		cout<<"*";
+//	}
+//	int space =2*n-2*i;
+//	for(int j =1; j<=space; j++){
+//		cout<<" ";
+//	}
+//	for(int j =1; j<=i; j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+//for(int i =n;i>=1; i--){
+//	for(int j =1; j<=i; j++){
+//		cout<<"*";
+//	}
+//	int space =2*n-2*i;
+//	for(int j =1; j<=space; j++){
+//		cout<<" ";
+//	}
+//	for(int j =1; j<=i; j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
+
+
+
+
