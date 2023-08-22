63e88226c3cd2b3560dfa4d7df108412e84a3704
Developer Baboo <babooheerani@gmail.com>
2023-08-22 16:09:21 +0500

    added further patterns

--- patterns.cpp
+++ patterns.cpp
@@ -120,19 +120,120 @@ int main() {
 
 
 //0-1 Pattern
-int n;
+//int n;
+//cin>>n;
+//for(int i =1; i<=n; i++){
+//	for(int j =1; j<=i; j++){
+//		if((j+i)%2 ==0 ){
+//			cout<<" 1";
+//		}
+//		else{
+//			cout<<" 0";
+//		}
+//	}
+//	cout<<endl;
+//}	
+
+
+//Rhombus Pattern
+
+//int n;
+//cin>>n;
+//
+//for(int i =1; i<=n; i++){
+//	for(int j=1; j<=n-i; j++){
+//		cout<<" ";
+//	}
+//	for(int j=1; j<=n; j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+
+// Number Pattern
+
+//int n;
+//cin>>n;
+//
+//for(int i =1; i<=n; i++ ){
+//	for(int j =1; j<=n-i; j++ ){
+//		cout<<" ";
+//	}
+//	
+//	for(int j =1; j<=i; j++){
+//		cout<<j<<" ";
+//	}
+//	cout<<endl;
+//}
+
+
+//int n;
+//cin>>n;
+//
+//for(int i =1; i<=n; i++){
+//	int j;
+//	for(j=1; j<=n-i; j++){
+//		cout<<" "<<" ";
+//	}
+//	int k=i;
+//	for(; j<=n; j++){
+//		cout<<k--<<" ";
+//	}
+//	k=2;
+//	for(; j<=n+i-1; j++){
+//		cout<<k++<<" ";
+//	}	
+//	cout<<endl;
+//}
+
+
+//Star Pattern
+//int n;
+//cin>>n;
+//for(int i =1; i<=n; i++){
+//	for(int j =1; j<=n-i; j++){
+//		cout<<" ";
+//	}
+//	
+//	for(int j=1; j<=2*i-1; j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+//
+//for(int i = n; i>=1; i--){
+//	for(int j =1; j<=n-i; j++){
+//		cout<<" ";
+//	}
+//	
+//	for(int j=1; j<=2*i-1; j++){
+//		cout<<"*";
+//	}
+//	cout<<endl;
+//}
+
+
+int n; 
 cin>>n;
-for(int i =1; i<=n; i++){
-	for(int j =1; j<=i; j++){
-		if((j+i)%2 ==0 ){
-			cout<<" 1";
+
+for(int i=1; i<=3; i++){
+	for(int j =1; j<=n; j++){
+		if((i+j)%4==0 || (i==2 && j%4 ==0)){
+			cout<<"*";	
 		}
 		else{
-			cout<<" 0";
+			cout<<" ";
 		}
 	}
 	cout<<endl;
-}	
+}
+
+
+
+
+
+
 
 
 
